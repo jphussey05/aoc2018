6a5c1fd96c34220cbbeab5f69974c8c3bd9b49c9
Jason Hussey <jphussey05@gmail.com>
2019-06-24 23:57:31 -0400

    creates class and initial parse

--- day10.py
+++ day10.py
@@ -1,14 +1,39 @@
+
+
+
 class Star(object):
     # represets each star
     # has start point, which is used only to find max size of grid
     # also has a velocity and current position
     # needs an update function to record a new location when moved
 
-    def __init__(self, ID):
-        pass
+    def __init__(self, start_x, start_y, x_move, y_move):
+        self.start_x = start_x
+        self.start_y = start_y
+        self.cur_x = start_x
+        self.cur_y = start_y
+        self.x_velocity = x_move
+        self.y_velocity = y_move
 
+    def move(self, step=1):
+        self.cur_x += (self.x_velocity * step)
+        self.cur_y += (self.y_velocity * step)
+
+    def __str__(self):
+        return f'Star at {self.cur_x}, {self.cur_y} w/ V: {self.x_velocity}, {self.y_velocity}'
+
+    def __repr__(self):
+        return (
+            f'Star({self.start_x},'
+            f'{self.start_y},'
+            f'{self.x_velocity},'
+            f'{self.y_velocity})'
+        )
 
 def find_max_xy():
+    '''
+    Take in all of the points to determine max xy for the grid size
+    '''
     pass
 
 def parse_start_points(contents):
@@ -16,9 +41,17 @@ def parse_start_points(contents):
     The initial strip of the ingested day10.txt file that has all of the points
     creates a list of Star objects
     '''
-    pass 
-
+    stars = []
+    for line in contents:
+        start, velocity = line.split(' velocity=')
+        x,y = map(int, start[start.find('<') + 1:start.find('>')].split(','))
+        dx, dy = map(
+            int, velocity[velocity.find('<') + 1: velocity.find('>')].split(',')
+            )
+        
+        stars.append(Star(x, y, dx, dy))
     
+    return stars
 
 
 
@@ -26,4 +59,6 @@ def parse_start_points(contents):
 with open('day10.txt') as fin:
     contents = fin.readlines()
 
-points = parse_start_points(contents)
+stars = parse_start_points(contents)
+for star in stars:
+    print(star)
